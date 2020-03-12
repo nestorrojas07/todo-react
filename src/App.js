8efe384d8e22f2917244db5ddf3ce4ef7d3beecd
Nestor <nestor.p@cheil.com>
2020-03-12 09:15:25 -0500

    My First Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import './App.css';
 
+
+function HelloWorld(){
+  return (
+    <div id="hello" >hello world</div>
+  );
+}
+
 function App() {
   return (
-    <div>hello world</div>
+    <div>This is the main component: <HelloWorld /></div>
   );
 }
 

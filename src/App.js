69c090f20a9d31b851530c3afce4d52b3df934ba
Nestor <nestor.p@cheil.com>
2020-03-13 08:38:38 -0500

    4. State and set State from event in Component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 
-function HelloWorld(props){
+/* function HelloWorld(props){
   return (
   <div id="hello" >
     <h1>{props.titulo}</h1>
@@ -10,14 +10,47 @@ function HelloWorld(props){
     </div>
   );
 }
+ */
+class HelloWorld extends React.Component{
+
+  //objeto para definir los estados propios de componente
+  state = {
+    //propiedades del componente
+    show : true
+  }
+
+  render() {
+    if(this.state.show){
+      return (
+        <div id="hello">
+          <h1>{this.props.titulo}</h1>
+          {this.props.texto}
+          <button onClick={() => this.toogleShow()}>Ocultar Forma 1</button>          
+        </div>
+      );
+    }
+    return (
+      <div onClick={() => this.setState({show : true})}>
+        <h1>show esta en false, Click Here to show </h1>
+      </div>
+    );
+  }
+  
+  toogleShow(){
+    const { show } = this.state;
+    this.setState( { show: !show});
+  }
+
+}
 
 function App(){
+
   return (
     <div>
         This is the main component: 
       <HelloWorld texto="Hello world Nestor" titulo ="English" /> 
       <HelloWorld texto="salut monde Nestor" titulo="France" />
-      <HelloWorld texto="Hola Mundo" titulo="Spainish" /> 
+      <HelloWorld texto="Hola Mundo" titulo="Spainish"  /> 
     </div>
     );
 }
